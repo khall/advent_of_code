39cb01ca033116a82a8fbf500f2c9a1aa75aebb2
Kevin Hall <kevin.hall@boundless.com>
2021-12-02 08:34:54 -0800

    2021 - 2

---
+++ 2021/2/a.rb
@@ -0,0 +1,26 @@
+class SubmarinePosition
+  def initialize
+    @input = File.readlines('input').map { |n| n.strip }
+  end
+
+  def final_position_product
+    horizontal_position = 0
+    vertical_position = 0
+
+    @input.each do |instruction|
+      direction, amount = instruction.split(' ')
+      case direction
+      when 'forward'
+        horizontal_position += amount.to_i
+      when 'down'
+        vertical_position += amount.to_i
+      when 'up'
+        vertical_position -= amount.to_i
+      end
+    end
+
+    horizontal_position * vertical_position
+  end
+end
+
+puts SubmarinePosition.new.final_position_product
